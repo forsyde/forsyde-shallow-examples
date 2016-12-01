02a154608215be2dc5b1a18ffab5fb4966be037f
José Edil Guimarães De Medeiros <j.edil@ene.unb.br>
2016-12-01 16:20:32 +0100

    Include Haddock documentation

--- src/ForSyDe/Examples/Untimed/Ascii2Bin.hs
+++ src/ForSyDe/Examples/Untimed/Ascii2Bin.hs
@@ -8,7 +8,8 @@
 -- Stability   :  experimental
 -- Portability :  portable
 --
--- This is the solution to Exercise 3.1 of Axel's book using the untimed MoC.
+-- This is the solution to Exercise 3.1 of Axel's book Modeling Embedded Systems
+-- and Soc's.
 -- 
 -----------------------------------------------------------------------------
 
@@ -18,31 +19,52 @@ module ForSyDe.Examples.Untimed.Ascii2Bin (char2bin,
 import ForSyDe.Shallow
 import Data.Char
 
--- p is a process that translates characters of the English alphabet and digits
--- into their binary ASCII representation.
+-- | 'char2bin' is a process that translates characters of the English alphabet
+-- and digits into their binary ASCII representation. It reverts the operation
+-- made by 'bin2char'.
+--
 -- For each firing, takes one token and generates 8 tokens.
-char2bin :: Signal Char -> Signal Char
+--
+-- >>> let sig = signal "A3b1"
+-- >>> char2bin sig
+-- {'0','1','0','0','0','0','0','1','0','0','1','1','0','0','1','1','0','1','1','0','0','0','1','0','0','0','1','1','0','0','0','1'}
+char2bin :: Signal Char -- ^ Input signal
+         -> Signal Char -- ^ Output signal
 char2bin = mapU 1 char2bin'
 
+-- | Auxiliary function to convert Chars into Strings of '0' and '1' 
 char2bin' :: [Char] -> [Char]
 char2bin' []     = []
 char2bin' (x:xs) = (reverse . take 8 . int2bin $ ord x) ++ (char2bin' xs)
 
--- Generates an infinite list of chars with little endian codification.
+-- | Auxiliary function that generates an infinite list of chars representing
+-- an integer in binary format with little endian codification.
 int2bin :: Int -> [Char]
 int2bin x   
     | mod x 2 == 0  = '0' : int2bin (div x 2)
     | otherwise     = '1' : int2bin (div (x - 1) 2)
 
--- q is a process that reverts the operation made by p.
+-- | 'bin2char is a process that translates characters in binary form back to
+-- an ascii representation. It reverts the operation made by 'char2bin'.
+--
 -- For each firing, takes 8 tokens and generates 1 token.
-bin2char :: Signal Char -> Signal Char
+--
+-- >>> let sig1 = signal "A3b1"
+-- >>> let sig2 = bin2char sig1
+-- >>> char2bin sig2
+-- {'A','3','b','1'}
+bin2char :: Signal Char -- ^ Input signal
+         -> Signal Char -- ^ Output signal
 bin2char = mapU 8 int2char
 
+-- | Auxiliary function that converts strings of '0' and '1' back to
+-- ascii representation.
 int2char :: [Char] -> [Char]
 int2char [] = []
 int2char x  = (chr $ bin2int $ take 8 x) : (int2char $ drop 8 x)
 
+-- | Auxiliary function that parses a string of '0' and '1' and converts it to
+-- Int.
 bin2int :: [Char] -> Int
 bin2int x = sum $ zipWith (*) string powers2
     where   powers2 = reverse $ map (2^) [0..7]
