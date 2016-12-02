cec6d2864dfb7e708fbe74149549040ad422362f
José Edil Guimarães De Medeiros <j.edil@ene.unb.br>
2016-12-02 18:48:44 +0100

    Add Haddock documentation.

--- src/ForSyDe/Examples/Synchronous/TrafficLight.hs
+++ src/ForSyDe/Examples/Synchronous/TrafficLight.hs
@@ -16,11 +16,24 @@ module ForSyDe.Examples.Synchronous.TrafficLight where
 
 import ForSyDe.Shallow
 
-
+-- | Clock data type.
 type Clock = Int
+-- | Internal register takes care of the actual state and how many
+-- tokens have arrived.
 type Register = (State, Count)
+-- | To keep track of how many tokens have arrived since last state transition.
 type Count = Int
-data State = RR1 | RY1 | RG | RY2 | RR2 | YR1 | GR | YR2
+-- | FSM states
+data State = RR1 -- ^ Both signals red.
+           | RY1 -- ^ Red / Yellow.
+           | RG  -- ^ Red / Green.
+           | RY2 -- ^ Red / Yellow.
+           | RR2 -- ^ Both signals red.
+           | YR1 -- ^ Yellow / Red.
+           | GR  -- ^ Green / Red.
+           | YR2 -- ^ Yellow / Red.
+
+-- | Possible outputs.        
 data Color = RED | YELLOW | GREEN
     deriving Show
 
@@ -39,7 +52,9 @@ trafficController :: Signal Int            -- ^ Clock signal
 trafficController = mooreSY nsf outf (RR1, 0)
 
 -- | Next state function. Implements the state diagram transitions.
-nsf :: Register -> Clock -> Register
+nsf :: Register                 -- ^ Actual state
+    -> Clock                    -- ^ Input token
+    -> Register                 -- ^ Next state
 nsf (RR1, cnt) c    | cnt < 1 = (RR1, cnt+1)
                     | otherwise = (RY1, 0)
 nsf (RY1, cnt) c    | cnt < 3 = (RY1, cnt+1)
@@ -57,8 +72,10 @@ nsf (GR, cnt)  c    | cnt < 60 = (GR, cnt+1)
 nsf (YR2, cnt) c    | cnt < 3 = (YR2, cnt+1)
                     | otherwise = (RR1, 0)
 
--- | Output function.
-outf :: Register -> (Color, Color)
+-- | Output function. This is a Moore machine, so the output depends
+-- only on the current state.
+outf :: Register                -- ^ State
+     -> (Color, Color)          -- ^ Output signal
 outf (RR1, cnt) = (RED, RED)
 outf (RR2, cnt) = (RED, RED)
 outf (RY1, cnt) = (RED, YELLOW)
